7e83effb41fa58851556ca48fdb6670432b60ad8
Siddhant <201951151@iiitvadodara.ac.in>
2021-10-09 11:55:30 +0530

    Added final components

--- src/App.js
+++ src/App.js
@@ -2,15 +2,18 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { ChatEngine} from 'react-chat-engine';
 import  ChatFeeds  from './components/ChatFeeds';
+import LoginForm from './components/LoginForm';
 import './App.css';
 function App() {
+  if (!localStorage.getItem('username')) return <LoginForm />;
+
   return (
     <div className="App">
       <ChatEngine 
         height = "100vh"
         projectID = "13bf235a-42db-4012-806f-9dd8da09daf6"
-        userName= "Siddhant"
-        userSecret= "Seedu@123"
+        userName= {localStorage.getItem('username')}
+        userSecret= {localStorage.getItem('password')}
         renderChatfeed = {(chatprops) => <ChatFeeds {...chatprops}/>}
       />
     </div>
--- src/components/ReceiveMessage.jsx
+++ src/components/ReceiveMessage.jsx
@@ -1,7 +1,29 @@
-const ReceiveMessage = () => {
+const ReceiveMessage = ({lastMessage, message}) => {
+    const isFirstMessageByUser = !lastMessage || lastMessage.sender.username !== MediaKeyMessageEvent.sender.username;
     return(
-        <div>
-            ReceiveMessage
+        <div className = "message-row">
+            {isFirstMessageByUser && (
+                <div 
+                className="message-avatar"
+                style={{ backgroundImage: message.sender && `url(${message.sender.avatar})` }}
+                />
+             )}
+            {message?.attachments?.length > 0 ? 
+                (
+                    <img
+                        src={message.attachments[0].file}
+                        alt="message-attachment"
+                        className="message-image"
+                        style={{ marginLeft: isFirstMessageByUser ? '4px' : '48px' }}
+                    />
+                )
+              :
+                (
+                    <div className="message" style={{ float: 'left', backgroundColor: '#CABCDC', marginLeft: isFirstMessageByUser ? '4px' : '48px' }}>
+                        {message.text}
+                    </div>
+                )
+            }
         </div>
     );
 }
